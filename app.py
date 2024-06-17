c9c0ed25aad821c1620b0598f1b207d6d7867513
Sugeng Riyanto <36408012+sugeng-riyanto@users.noreply.github.com>
2024-06-17 09:31:23 +0700

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def init_db():
     conn.close()
 def generate_unique_link():
     unique_id = str(uuid.uuid4())
-    return f"http://localhost:8501/?id={unique_id}", unique_id
+    return f"https://mysignature.streamlit.app/?id={unique_id}", unique_id
 
 def save_data(unique_id, data):
     conn = sqlite3.connect('form_data.db')
